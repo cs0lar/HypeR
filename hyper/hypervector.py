03371750bf31b5700c9f23d9228d7f6ae4dd3aa1
cs0lar <cristiano.solarino@brightminded.com>
2022-04-19 12:04:21 +0100

    added support for bipolar hypervectors

--- hyper/hypervector.py
+++ hyper/hypervector.py
@@ -5,6 +5,105 @@ import numpy as np
 from bitarray import bitarray
 from bitarray.util import count_xor
 
+class Permutation:
+	"""
+	Utility class to generate fixed, pre-computed permutations as lists
+	of integers. When encoding sequences in Hyperdimensional computing
+	a permutation operator is applied multiple times to a given vector
+	depending on its position in the sequence. For example, if an n-gram
+	of length 4 like GRAB is being encoded, the vector corresponding to 
+	the G character will be permuted three times: \rho( \rho( \rho( G ) ) ). 
+	Note that the permutation is fixed and simply applied multiple times.
+	After the `generate` method is executed, the fixed, base permutation 
+	can be retrieved at index 0 of the `Permutation` object: i.e. `perm[0]`.
+	If `n` > 1 is supplied to the `generate` method, then `perm[ i ]` for 
+	`i` \in { 1, ..., n-1 } will return a permutation that when applied 
+	to input X is equivalent to applying the base permutation on
+	X i+1 times, for example ( with a slight abuse of notation ), 
+	`perm[ 1 ]`( X ) = `perm[ 0 ]`( `perm[ 0 ]`( X ) ).
+
+	Attributes
+	----------
+
+	_rng : Generator
+		A random number generator for generating random permutations. Default is `None`.
+
+	_perms : list 
+		A list where each element is a permutation in the form of a list {\rho(0}, ..., \rho(m}}
+		where \rho: {1, ..., m} -> {1, ..., m}.
+
+	Methods
+	-------
+
+	generate( d, n )
+		It generates a base permutation \rho of length `d` and pre-computes additional n-1
+		compositions of the base permutation such that `perm[ i ]`( X ) = \rho^{i+1}( X )
+		e.g. `perm[ 2 ]`( X ) = \rho( \rho( \rho( X ) ) ).
+ 
+	"""
+
+	def __init__( self, rng=None ):
+		"""
+		Creates a new `Permutation` using an optionally 
+		given random number `Generator`.
+
+		Parameters
+		----------
+
+		rng : Generator
+			A `Generator` to use for generating random permutations. 
+			If `None` is passed, a default `Generator` is instantiated.
+
+		"""
+		if rng is None:
+			rng = np.random.default_rng()
+
+		self._rng = rng
+
+
+	def generate( self, d, n ):
+		"""
+		It generates a base permutation \rho of length `d` and pre-computes additional n-1
+		compositions of the base permutation such that `perm[ i ]`( X ) = \rho^{i+1}( X )
+		e.g. `perm[ 2 ]`( X ) = \rho( \rho( \rho( X ) ) ).
+
+		Parameters
+		----------
+
+		d : int
+			The dimensionality of the permutation map.
+
+		n : int
+			The number of additional permutations to pre-compute.
+
+
+		Returns
+		-------
+
+		Permutation
+			It returns itself after the underlying list of permutations has been computed.
+
+		"""
+		perm = self._rng.permutation( d )
+		self._perms = [ np.zeros( ( 1, d ), dtype=int ) ]
+
+		self._perms[ 0 ] = perm
+
+		[ self._perms.append( self._perms[ i ][ perm ] ) for i in range( n - 1 ) ]
+
+		return self
+
+
+	def __getitem__( self, i ):
+
+		return self._perms[ i ]
+
+
+	def __len__( self ):
+
+		return len( self._perms )
+
+
 class Hypervector:
 	"""
 	Base class for all hyper vector types ( e.g. binary, bipolar, integer )
@@ -20,6 +119,34 @@ class Hypervector:
 
 		self._hv = hv
 
+	@staticmethod
+	def check( clazz, *args ):
+		"""
+		Static utility to check whether an arbitrary number of objects 
+		are instances of the given Hypervector subclass.
+
+		Parameters
+		----------
+
+		clazz : str
+			The name of the subclass the input objects should be instances of.
+
+		*args : Hypervectors
+			An arbitrary number of objects to be checked.
+
+		Returns
+		-------
+
+		boolean:
+			True if all input objects are instances of the same, given 
+			Hypervector subclass. False otherwise.
+
+		"""
+
+		isSubclass = [ issubclass( arg.__class__, Hypervector ) for arg in args ]
+		isClass = [ ( arg.__class__.__name__ == clazz ) for arg in args ]
+		
+		return all( isSubclass + isClass )
 
 
 class BinaryHypervector( Hypervector ):
@@ -214,19 +341,23 @@ class BinaryHypervector( Hypervector ):
 			sum of the input BinaryHypervectors.
 
 		"""
-		vecs = [ arg for arg in args if isinstance( arg, BinaryHypervector ) ]
 
-		l = len( vecs[ 0 ] )
+		if Hypervector.check( 'BinaryHypervector', *args ):
 
-		if ( len( vecs ) % 2 ) == 0:
-			# to avoid 0 bias when the number of hypervectors to sum 
-			# is even, we add a random hypervector to make the number odd.
-			vecs.append( BinaryHypervector.new( l ) )
+			vecs = [ arg for arg in args if isinstance( arg, BinaryHypervector ) ]
 
-		hv = bitarray( [ BinaryHypervector.majority( bitarray( [ vec[ i ] for vec in vecs ] ) ) for i in range( l ) ] )
+			l = len( vecs[ 0 ] )
 
-		return BinaryHypervector( hv=hv )
+			if ( len( vecs ) % 2 ) == 0:
+				# to avoid 0 bias when the number of hypervectors to sum 
+				# is even, we add a random hypervector to make the number odd.
+				vecs.append( BinaryHypervector.new( l ) )
 
+			hv = bitarray( [ BinaryHypervector.majority( bitarray( [ vec[ i ] for vec in vecs ] ) ) for i in range( l ) ] )
+
+			return BinaryHypervector( hv=hv )
+
+		raise ValueError( 'All inputs must be BinaryHypervectors' )
 
 	@staticmethod
 	def mul( A, B ):
@@ -251,10 +382,13 @@ class BinaryHypervector( Hypervector ):
 
 		"""
 
-		xor = A._hv ^ B._hv
+		if Hypervector.check( 'BinaryHypervector', A, B ):
+			
+			xor = A._hv ^ B._hv
 
-		return BinaryHypervector( hv=xor )
+			return BinaryHypervector( hv=xor )
 		
+		raise ValueError( 'A and B must be BinaryHypervectors' )
 
 	@staticmethod
 	def hamming( X, Y ):
@@ -279,10 +413,14 @@ class BinaryHypervector( Hypervector ):
 			when `hamming( X, Y )` = 1.
 
 		"""
-		d = len( X )
 
-		return count_xor( X._hv, Y._hv ) / d
+		if Hypervector.check( 'BinaryHypervector', X, Y ):
+			
+			d = len( X )
 
+			return count_xor( X._hv, Y._hv ) / d
+
+		raise ValueError( 'X and Y must be BinaryHypervectors' )
 
 	def __str__( self ):
 		"""
@@ -320,5 +458,266 @@ class BinaryHypervector( Hypervector ):
 
 		raise False
 
+
+class BipolarHypervector( Hypervector ):
+	"""
+	Implementation of a bipolar hypervector X where the value of a component x_i is in {-1, 1}. 
+	It uses a numpy array as the underlying hypervector data type.
+
+	Attributes
+	----------
+
+	_hv: ndarray
+		The actual underlying hypervector in its numpy array form.
+
+
+	Methods
+	-------
+
+	new( d, rng )
+		Static method that creates a new random Bipolar Hypervector of size d.
+
+	sum( *args )
+		Static method that computes the sum of a variable
+		number of BipolarHypervectors.
+
+	permute( rng )
+		It permutes the underlying numpy array 
+
+	cosine( X, Y )
+		Static method that computes the cosine similarity between two bipolar hypervectors.
+
+	mul( A, B )
+		Static method that performs the binding operation which for bipolar hypervectors 
+		takes of element-wise product.
+
+	
+	"""
+	def __init__( self, hv ):
+
+		super().__init__( hv )
+
+
+	def new( d, rng ):
+		"""
+		It creates a new random BipolarHypervector using a numpy
+		array as underlying data type. Each element of the vector
+		has a value in {-1, 1}.
+	
+		Parameters
+		----------
+
+		d : int
+			The dimensionality of the hyper vector e.g. 10,000
+
+		rng : numpy.random.Generator
+			The random number generator to use for creating the vector.
+
+		Returns
+		-------
+
+		BipolarHypervector
+			A random bipolar hypervector.
+
+		"""
+		v = [-1, 1 ]
+
+		hv = np.array( [ v[ i ] for i in rng.integers( 2, size=d ) ] )
+
+		return BipolarHypervector( hv=hv )
+
+
+	@staticmethod
+	def cosine( X, Y ):
+		"""	
+		It computes the cosine similarity between two bipolar hypervectors X and Y.
+
+		Parameters
+		----------
+
+		X : BipolarHypervector
+			A bipolar hypervector
+
+		Y : BipolarHypervector
+			A bipolar hypervector
+
+		Returns
+		-------
+		
+		float
+			The cosine similarity between X and Y. If `cosine( X, Y )` = 0 then
+			X and Y are orthogonal. X and Y are identical when `cosine( X, Y )` = 1.
+
+		"""
+
+		if Hypervector.check( 'BipolarHypervector', X, Y ):
+			
+			normX = np.linalg.norm( X._hv )
+			normY = np.linalg.norm( Y._hv )
+
+			return np.dot( X._hv, Y._hv ) / ( normX * normY )
+
+		raise ValueError( 'X and Y must be BipolarHypervectors' )
+
+
+	def __len__( self ):
+		"""
+		It overrides the `__len__` magic function so that 
+		the `len` function can be invoked on a BipolarHypervector.
+
+		Returns
+		-------
+		
+		int
+			The length of the underlying numpy array.
+
+		"""
+		return len( self._hv )
+
+
+	def permute( self, perm, c=1 ):
+		"""
+		It computes a permutation of the underlying numpy array based on 
+		the given, fixed `Permutation` `perm`. The fixed permutation is 
+		applied `c` times.
+
+		Parameters
+		----------
+
+		perm: Permutation
+			A `Permutation` object supplying permutations as lists of
+			integers of length equal to the dimensionality of this hypervector.
+
+		c : int
+			The number of permutations to apply.
+
+		Returns
+		-------
+
+		BipolarHypervector
+			The permuted hypervector. The permutation occurs in-place.
+
+		"""
+
+		if not isinstance( perm, Permutation ):
+
+			raise ValueError( 'perm must be an instance of Permutation' )
+
+		if c >= 1:
+
+			permutation = perm[ c-1 ]
+
+			if len( permutation ) != len( self ):
+
+				raise ValueError( f'perm must contain permutations of length {len( self )}' )
+
+			self._hv = self._hv[ permutation ]
+
+		return self
+
+	def __str__( self ):
+		"""
+		It overrides the magic `__str__` function in order to pretty print 
+		the underlying numpy array.
+
+		Returns
+		-------
+
+		str
+			The content of the underlying numpy array in string format.
+
+		"""
+		return str( self._hv.tolist() )
+
+	def __eq__( self, other ):
+		"""
+		It overrides the magic `__eq__` function in order to support
+		comparison of two bipolar hypervectors.
+
+		Parameters
+		----------
+
+		other : BipolarHypervector
+			A bipolar hypervector to compare with self.
+
+		Returns
+		-------
+
+		boolean
+			`True` if the underlying numpy arrays are equal, `False` otherwise.
+		
+		"""
+		
+		if isinstance( other, BipolarHypervector ):
+		
+			return np.array_equal( self._hv, other._hv )
+
+		raise False
+
+
+	@staticmethod
+	def sum( *args ):
+		"""
+		It computes the thresholded, element-wise sum of an arbitrary number of 
+		BipolarHypervectors.
+
+		Parameters
+		----------
+		
+		*args : BipolarHypervectors
+			A variable number of BipolarHypervectors.
+
+		Returns
+		-------
+
+		BipolarHypervector
+			A new BipolarHypervector representing the thresholded, element-wise sum 
+			of the input BipolarHypervectors.
+
+		"""
+
+		if Hypervector.check( 'BipolarHypervector', *args ):
+
+			hv = np.vstack( [ arg._hv for arg in args ] )
+			
+			hv = np.sum( hv, axis=0 )
+
+			hv[ hv > 0 ] = 1
+			hv[ hv <= 0 ] = -1
+			
+			return BipolarHypervector( hv=hv )
+
+		raise ValueError( 'All inputs must be BipolarHypervectors' )
+
+	@staticmethod
+	def mul( A, B ):
+		"""
+		It performs a binding operation, binding A and B together for form X = A*B.
+		For bipolar hypervectors the binding operation is element-wise multiplication.
+
+		Parameters
+		----------
+
+		A : BipolarHypervector
+			A bipolar hypervector
+
+		B : BipolarHypervector
+			A bipolar hypervector
+
+		Returns
+		-------
+
+		BipolarHypervector
+			A * B
+
+		"""
+
+		if Hypervector.check( 'BipolarHypervector', A, B ):
+			
+			return BipolarHypervector( hv=np.multiply( A._hv, B._hv ) )
+		
+		raise ValueError( 'A and B must be BipolarHypervectors' )
+
+		
 
 
--- tests/hypervector.py
+++ tests/hypervector.py
@@ -6,11 +6,12 @@ from bitarray import bitarray
 
 from tqdm import tqdm
 
-from hyper.hypervector import BinaryHypervector
+from hyper.hypervector import Permutation, BinaryHypervector, BipolarHypervector
 
 import matplotlib.pyplot as plt
 
 from scipy.stats import norm
+from scipy.spatial import distance
 
 
 class TestBinaryHipervector( unittest.TestCase ):
@@ -117,7 +118,131 @@ class TestBinaryHipervector( unittest.TestCase ):
 		plt.legend( labels )
 		plt.ylim( 0, 100 )
 		plt.xticks( np.linspace( 0, 1., 5 ) )
-		plt.savefig( 'testnew.png' )
+		plt.savefig( 'testbinarynew.png' )
 
 
+
+class TestBipolaripervector( unittest.TestCase ):
+
+	def testNew( self ):
+		"""
+		It must be demonstrated that as the dimensionality of the
+		hypervectors increases so does the probability that the 
+		cosine distance between two random hyper vectors approaches 0, i.e.
+		any two random vectors are most likely orthogonal. 
+		"""
+
+		dims = [ 100, 500, 2500, 10000 ]
+		colors = [ 'r', 'b', 'g', 'c', 'm' ]
+		labels = [ f'd={v}' for v in dims ]
+		iters = 1500
+
+		rng = np.random.default_rng()
+
+		def sample():
+
+			X = BipolarHypervector.new( d, rng )
+			Y = BipolarHypervector.new( d, rng )
+
+			return BipolarHypervector.cosine( X, Y )
+
+		for i, d in enumerate( dims ): 
+			samples = [ sample() for i in tqdm( range( iters ) ) ]
+
+			
+			mean = np.mean( samples )
+			std = np.std( samples )
+
+			x = np.linspace( mean - 3. * std, mean + 3. * std, 100 )
+
+			plt.plot( x, norm.pdf( x, mean, std ), colors[ i ] )
+
+		plt.xlabel( 'cosine similarity' )
+		plt.ylabel( 'Probability (%)' )
+		plt.legend( labels )
+		plt.ylim( 0, 100 )
+		plt.xticks( np.linspace( -.5, .5, 5 ) )
+		plt.savefig( 'testbipolarnew.png' )
+
+
+	def testPermute( self ):
+
+		numPerms = 4
+		dims = 10000
+
+		rng = np.random.default_rng()
+
+		perms = Permutation( rng=rng )
+
+		perms.generate( dims, numPerms )
+
+		basePerm = perms[ 0 ]
+
+		hv = BipolarHypervector.new( dims, rng )._hv
+		v = np.array( hv )
+
+		for i in range( numPerms ):
+			
+			w = BipolarHypervector( hv=hv )
+
+			self.assertEqual( w.permute( perms, c=i ), BipolarHypervector( hv=v ) )
+
+			v = v[ basePerm ]
+
+	def testCosine( self ):
+
+		A = BipolarHypervector( hv=np.array( [ -1, -1, -1, -1,  1,  1, -1, -1,  1, 1 ] ) )
+		B = BipolarHypervector( hv=np.array( [  1, -1,  1,  1, -1, -1, -1,  1, -1, 1 ] ) )
+
+		self.assertAlmostEqual( BipolarHypervector.cosine( A, A ), 1. )
+
+		expected = 1. - distance.cosine( A._hv, B._hv )
+		actual = BipolarHypervector.cosine( A, B )
+
+		self.assertAlmostEqual( actual, expected )
+
+
+	def testSum( self ):
+
+		A = BipolarHypervector( hv=np.array( [ -1, -1, -1, -1,  1,  1, -1, -1,  1, 1 ] ) )
+		B = BipolarHypervector( hv=np.array( [  1, -1,  1,  1, -1, -1, -1,  1, -1, 1 ] ) )
+		C = BipolarHypervector( hv=np.array( [ -1, -1,  1, -1,  1, -1,  1,  1, -1, 1 ] ) )
+	
+		expected = BipolarHypervector( hv=np.array( [ -1, -1, 1, -1, 1, -1, -1, 1, -1, 1 ] ) ) 
+
+		actual = BipolarHypervector.sum( A, B, C )
+
+		self.assertEqual( actual, expected )
+		
+		# The sum (and the mean) of random hypervectors has the
+		# property that it is similar to each of the
+		# hypervectors being added together. 
+
+		dims = 10000
+
+		rng = np.random.default_rng()
+		
+		scores = []
+
+		A, B, C = [ BipolarHypervector.new( dims, rng ) for i in range( 3 ) ] 
+
+		Z = BipolarHypervector.sum( A, B, C )
+
+		for V in [ A, B, C ]:
+
+			scores.append( BipolarHypervector.cosine( Z, V ) )
+		
+		self.assertTrue( np.average( scores ) > .4 )
+
+	def testMul( self ):
+
+		A = BipolarHypervector( hv=np.array( [ -1, -1, -1, -1,  1,  1, -1, -1,  1, 1 ] ) )
+		B = BipolarHypervector( hv=np.array( [  1, -1,  1,  1, -1, -1, -1,  1, -1, 1 ] ) )
+		
+		expected = A._hv * B._hv
+		actual = BipolarHypervector.mul( A, B )
+
+		self.assertEqual
+
+		
 
