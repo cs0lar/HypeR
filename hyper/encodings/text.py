eb1b54171b66acb5899889938ce4b240796098e6
cs0lar <cristiano.solarino@brightminded.com>
2022-04-26 14:56:07 +0100

    cleaned up and added docs to encoding functionality

--- hyper/encodings/text.py
+++ hyper/encodings/text.py
@@ -1,20 +1,26 @@
-import time, sys, re
-
 import numpy as np
-
-from tqdm import tqdm
-
-import matplotlib.pyplot as plt
-
-from unidecode import unidecode
-
-from sklearn.manifold import TSNE
 
 from hyper.hypervector import BipolarHypervector
 
 
 class Alphabet():
+	"""
+	This class allows to represent a collection of symbols ( e.g. letters, words, etc )
+	as random hypervectors. By default it will generate random bipolar hypervectors
+	for the 26 ASCII characters corresponding to the lower-case english alphabet letters
+	plus the space character. These "atomic" hypervectors can then be combined together to 
+	encode sets, sequences and bindings of the alphabet symbols according to the
+	basic operations on hypervectors.
+	Once an instance is constructed it can be used like a dictionary, retrieving the 
+	hypervector for a particular symbol by using that symbols as an indexing key.
 
+	Attributes
+	----------
+
+	ALPHABET : array
+		A static list of the 26 ASCII english alphabet characters plus the space character.
+
+	"""
 	ALPHABET = [ chr( 32 ) ] + [ chr( i ) for i in range( 97, 123 ) ]
 
 	def __init__( self, d, rng, alphabet=ALPHABET ):
@@ -32,7 +38,34 @@ class Alphabet():
 
 class TextEncoding():
 	"""
-	Implementation of https://link.springer.com/chapter/10.1007/978-3-319-52289-0_21
+	It implements a Text hypervector as described in
+	https://redwood.berkeley.edu/wp-content/uploads/2020/08/JoshiEtAl-QI2016-language-geometry-copy.pdf.
+	A piece of text is encoded as the hypervector sum of its ngrams. An ngram 
+	is a sequence of n consecutive characters extracted from the text and itself encoded as 
+	an hypervector using multiplication and permutation.
+
+	Attributes
+	----------
+
+	_alphabet : Alphabet
+		An instance of `Alphabet` containing the hypervector representation of the appropriate language alphabet's symbols.
+
+	_n :  int
+		The desired ngram size.
+
+	Methods
+	---------
+
+	encodengram( ngram ):
+		It encodes a single ngram ( a sequence of n consecutive characters from the text ) 
+
+	encodeblock( v, w, n, A ):
+		It encodes a single ngram given the previous ngram and the new character from this
+		block to be added.
+
+	encode( text ):
+		It encodes a all textual string using ngram and text hypervector encoding.	
+
 	"""
 	def __init__( self, alphabet, n ):
 		
@@ -44,7 +77,29 @@ class TextEncoding():
 		self._n = n 
 
 	def encodengram( self, ngram ):
+		"""
+		It encodes a single ngram ( a sequence of n consecutive characters from the text ) using 
+		hypevector sequence encoding. Let \rho be the permutation operation, then 
+		the ngram `GRAB` is encoded by performing the following hypervector computation:
 
+			\rho( \rho ( \rho( G ) ) ) * \rho( \rho( R ) ) * \rho( A ) * B
+
+		where G, R, A and B are hypervector representations of the ASCII characters 
+		`g`, `r`, `a` and `b` respectively.
+	
+		Parameters
+		----------
+
+		ngram : string
+			A string of `n` characters.
+
+		Returns
+		-------
+
+		BipolarHypervector
+			A `BipolarHypervector` encoding the ngram sequence.
+
+		"""
 		def mul( A, B ):
 			A._hv = BipolarHypervector.mul( A, B )._hv
 
@@ -63,19 +118,38 @@ class TextEncoding():
 			raise ValueError( f'key {e.args[ 0 ]} not in alphabet' )
 
 
-	def encode( self, entity ):
+	def encode( self, text ):
+		"""
+		It encodes a textual string as the sum of all its ngram sequences. Let the text be
+		'with great powers' and n=4 be the ngram size. The collection of ngrams is
+		[ 'with' , 'ith ', 'th g', 'h gr', ' gre', 'grea', 'reat', 'eat ', 'at p', 't po', ' pow', 'powe', 'ower', 'wers' ]
+		Each of the 14 ngrams is encoded as a hypervector using permutations and multiplications ( see `encodengram` )
+		and these hypervectors are summed together to obtain a single hypervector representing the entire text.
 
-		if not isinstance( entity, str ):
+		Parameters
+		----------
 
-			raise ValueError( 'entity must be a string' )
+		text: string
+			The text to be encoded.
 
-		size = len( entity )
+		Returns
+		-------
+
+		BipolarHypervector
+			A `BipolarHypervector` encoding the text.
+
+		"""
+		if not isinstance( text, str ):
+
+			raise ValueError( 'text must be a string' )
+
+		size = len( text )
 
 		if size < self._n:
 
-			raise ValueError( 'size of entity is less than size of ngram' )
+			raise ValueError( 'size of text is less than size of ngram' )
 
-		text = f' {entity.lower().strip( " " )} '
+		text = text.lower().strip( " " )
 
 		start = 0
 		step = self._n
@@ -86,13 +160,35 @@ class TextEncoding():
 		# the last block we computed
 		V = BipolarHypervector( hv=A._hv )
 		
-		[ A.add( self.encodeblock( text[ i ], text[ i+step ], step, V ) ) for i in tqdm( range( len( text )-step  ) ) ]
+		[ A.add( self.encodeblock( text[ i ], text[ i+step ], V ) ) for i in tqdm( range( len( text )-step  ) ) ]
 
 		return A.threshold()
 
-	def encodeblock( self, v, w, n, A ):
+	def encodeblock( self, v, w, A ):
+		"""
+		It implements a fast ngram encoding algorithm that can be used
+		to encode the m+1 th ngram given the m th ngram hypervector ( refer to section 2.2 of paper ).
+		Note that this function actually modifies `A` to be the m+1 th ngram encoding 
+		and returns it.
 
-		V = self._alphabet[ v ].permute( c=( n-1 ) )
+		Parameters
+		----------
+
+		v - string
+			The first character of the m th ngram.
+
+		w - string
+			The last character for the m+1 th ngram.
+
+		A - BipolarHypervector
+			The hypervector representation of the m th ngram.
+
+		Returns
+		--------
+			A `BipolarHypervector` encoding the m+1 th ngram.
+
+		"""
+		V = self._alphabet[ v ].permute( c=( self._n-1 ) )
 		W = self._alphabet[ w ]
 
 		V = BipolarHypervector.mul( V, A )
@@ -103,99 +199,3 @@ class TextEncoding():
 
 
 
-if __name__ == '__main__':
-	
-	langs = [
-
-		'bul_news_2020_30K',
-		'ces_news_2020_30K',
-		'dan_news_2020_30K',
-		'deu_news_2021_30K',
-		'ell_news_2020_30K',
-		'eng_news_2020_30K',
-		'est_news_2020_30K',
-		'fin_news_2020_30K',
-		'fra_news_2020_30K',
-		'hun_news_2020_30K',
-		'ita_news_2020_30K',
-		'lav_news_2020_30K',
-		'lit_news_2020_30K',
-		'nld_news_2020_30K',
-		'pol_news_2020_30K',
-		'por_news_2020_30K',
-		'ron_news_2020_30K',
-		'slk_news_2020_30K',
-		'slv_news_2020_30K',
-		'spa_news_2020_30K',
-		'swe_news_2020_30K'	
-	
-	]
-
-	labels = [
-
-		'bul', 'ces', 'dan', 'deu', 'ell', 'eng',
-		'est', 'fin', 'fra', 'hun', 'ita', 'lav',
-		'lit', 'nld', 'pol', 'por', 'ron', 'slk',
-		'slv', 'spa', 'swe'
-
-	]
-	
-	suffix = '-sentences.txt'
-
-	d = 10000
-	n = 4
-	l = len( langs )
-	rng = np.random.default_rng(  )
-
-	alphabet = Alphabet( d=d, rng=rng )
-
-	encoding = TextEncoding( alphabet, n )
-
-	regex = re.compile( r'[\W\d\s]+' )
-
-	def process( string, alphabet ):
-
-		unidecoded = unidecode( string ).lower()
-		unidecoded = regex.sub( ' ', unidecoded )
-		out = ''.join( c for c in unidecoded )
-
-		return out
-
-	base = sys.argv[ 1 ]
-	L = []
-
-	for i, lang in enumerate( langs ):
-		with open( f'{base}/{lang}/{lang}{suffix}', 'r' ) as f:
-		
-			lines = [ process( line.split( '\t' )[ 1 ].lower(), Alphabet.ALPHABET ) for line in f ]
-		
-		text = ' '.join( lines )[ :1000000 ]
-
-		textVector = encoding.encode( text ) 
-		L.append( textVector )
-
-	D = np.zeros( ( l, l ) )
-
-	for i in range( l ):
-		for j in range( l ):
-			D[ i, j ] = BipolarHypervector.cosine( L[ i ], L[ j ] )
-	
-	embed = TSNE( n_components=2, learning_rate='auto', metric='precomputed', init='random', square_distances=True ).fit_transform( D )
-
-	markers = [ 
-
-		'o', 'v', '^', '<', '>', '1', '2', '3', 
-		'4', '8', 's', 'p', '*', 'h', 'H', '+',
-		'x', 'X', 'D', 'd', '|'
-
-	]
-
-	cmap = plt.cm.get_cmap( 'hsv', l )
-
-	for i in range( l ):
-		x, y = embed[ i ]
-		plt.scatter( x, y, color=cmap( i ), alpha=0.5, marker=markers[ i ] )
-	
-	plt.legend( labels )
-
-	plt.show()
